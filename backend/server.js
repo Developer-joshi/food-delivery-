8e7fa1739d22aeaad6deb6206ae4cc913fca097b
Developer-joshi <sarveshjoshi017@gmail.com>
2025-06-03 22:31:09 +0530

    added backend folder structure and server.js

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+ //es6 syntax
+ import express  from "express"
+ import cors from 'cors'
+ 
+ // app config
+ const app = express()
+ const port = process.env.PORT || 4000;
+ 
+ 
+ // middlewares
+ app.use(express.json())
+ app.use(cors())
+ 
+ // db connection
+//  connectDB()
+ 
+ // api endpoints
+ 
+ app.get("/", (req, res) => {
+     res.send("API Working")
+   });
+ 
+ app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
